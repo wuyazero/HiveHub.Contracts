4dec7734fae63845eb2e456e7148780600b96ce8
blockchaindev91 <hisamitsuteru@gmail.com>
2022-09-16 10:46:29 +0900

    fix: update interface

--- contracts/interface/INodeRegistry.sol
+++ contracts/interface/INodeRegistry.sol
@@ -10,13 +10,11 @@ interface INodeRegistry is IERC721 {
         uint256 tokenId;
         string tokenURI;
         string nodeEntry;
-        address receiptAddr;
         address ownerAddr;
     }
     function mint(uint256 tokenId, string memory tokenURI, string memory nodeEntry) external payable;
-    function mint(uint256 tokenId, string memory tokenURI, string memory nodeEntry, address receiptAddr) external payable;
     function burn(uint256 tokenId) external;
-    function updateNode(uint256 tokenId, string memory tokenURI, address receiptAddr) external;
+    function updateNode(uint256 tokenId, string memory tokenURI) external;
     function reveal() external;
     function isRevealed() external view returns (uint256);
     function nodeInfo(uint256 tokenId) external view returns (Node memory);
@@ -30,7 +28,7 @@ interface INodeRegistry is IERC721 {
     function setPlatformFee(address platformAddr, uint256 platformFee) external;
     function getPlatformFee() external view returns (address platformAddress, uint256 platformFee);
     event RegisteredFees(uint256 tokenId, address platformAddr, uint256 registerFee);
-    event NodeRegistered(uint256 tokenId, string tokenURI, string nodeEntry, address receiptAddr, address ownerAddr);
+    event NodeRegistered(uint256 tokenId, string tokenURI, string nodeEntry, address ownerAddr);
     event NodeUnregistered(uint256 tokenId);
     event NodeUpdated(uint256 tokenId, string newNodeURI);
     event Revealed(uint256 state);
