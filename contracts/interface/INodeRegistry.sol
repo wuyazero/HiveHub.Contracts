f1d71bc9d4f90942a954b6fe513d21bc10197e14
blockchaindev91 <hisamitsuteru@gmail.com>
2022-09-16 14:40:53 +0900

    fix: update interface

--- contracts/interface/INodeRegistry.sol
+++ contracts/interface/INodeRegistry.sol
@@ -10,7 +10,6 @@ interface INodeRegistry is IERC721 {
         uint256 tokenId;
         string tokenURI;
         string nodeEntry;
-        address ownerAddr;
     }
     function mint(uint256 tokenId, string memory tokenURI, string memory nodeEntry) external payable;
     function burn(uint256 tokenId) external;
@@ -28,7 +27,7 @@ interface INodeRegistry is IERC721 {
     function setPlatformFee(address platformAddr, uint256 platformFee) external;
     function getPlatformFee() external view returns (address platformAddress, uint256 platformFee);
     event RegisteredFees(uint256 tokenId, address platformAddr, uint256 registerFee);
-    event NodeRegistered(uint256 tokenId, string tokenURI, string nodeEntry, address ownerAddr);
+    event NodeRegistered(uint256 tokenId, string tokenURI, string nodeEntry);
     event NodeUnregistered(uint256 tokenId);
     event NodeUpdated(uint256 tokenId, string newNodeURI);
     event Revealed(uint256 state);
